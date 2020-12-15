90c383012490d0cb0ab151b865f8f611a83e35e5
Davi Cid <davicid123@gmail.com>
2020-12-15 13:50:23 -0300

    Resolução de problemas em PHP

---
+++ Problema7/funcao_media_notas.php
@@ -0,0 +1,26 @@
+<?php
+
+$notas = [NULL, NULL, NULL];
+
+$notas[0] = $_POST["nota1"];
+$notas[1] = $_POST["nota2"];
+$notas[2] = $_POST["nota3"];
+
+echo "N1 = " . $notas[0];
+echo "<br/>";
+echo "N2 = " . $notas[1];
+echo "<br/>";
+echo "N3 = " . $notas[2];
+echo "<br/>";
+
+function calcularMedia($notas)
+{
+    if ($notas[0] && $notas[1] && $notas[2] != NULL) {
+        $notaMedia = ($notas[0] + $notas[1] + $notas[2]) / 3;
+        return round($notaMedia, 1);
+    } else {
+        return NULL;
+    }
+}
+
+echo "A nota media do aluno = " . calcularMedia($notas);
